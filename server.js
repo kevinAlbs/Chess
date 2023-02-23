683b03bdff2a1645bd376b822af917de86c4cd94
Kevin Albertson <kevin.albertson@10gen.com>
2023-02-22 20:56:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var app = require('http').createServer(handler),
 io = require('socket.io').listen(app),
-ws = require("ws"),
 fs = require("fs"),
 url = require("url"),
 port = process.env.PORT || 5000,
